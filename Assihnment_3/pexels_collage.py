64f1b34a6c78005610fcdaaab7f1886a0684dbd9
Anshu Jangid <anshu.jangid@intimetec.com>
2023-12-21 10:26:01 +0530

    collage assignment

--- Assihnment_3/pexels_collage.py
+++ Assihnment_3/pexels_collage.py
@@ -2,33 +2,25 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-def get_pexels_images(api_key, query, num_images=5):
+def get_pexels_images(api_key, query, num_images):
     url = f'https://api.pexels.com/v1/search?query={query}&per_page={num_images}'
     headers = {'Authorization': api_key}
-
     response = requests.get(url, headers=headers)
     data = response.json()
-
     images = []
     for photo in data.get('photos', []):
         images.append(photo['src']['large'])
-
+        # print("image", images)
     return images
 
 def create_image_collage(images, output_path='collage.jpg'):
     images_data = [Image.open(BytesIO(requests.get(image).content)) for image in images]
-
-    # Calculate collage size based on the first image
     collage_width = images_data[0].width * len(images)
     collage_height = images_data[0].height
-
-    # Create a new image with the calculated size
     collage = Image.new('RGB', (collage_width, collage_height))
-
-    # Paste each image into the collage
     x_offset = 0
     for img in images_data:
-        collage.paste(img, (x_offset, 0))
+        collage.paste(img, (x_offset,100))
         x_offset += img.width
 
     # Save the collage
@@ -36,10 +28,8 @@ def create_image_collage(images, output_path='collage.jpg'):
     collage.show()
 
 if __name__ == '__main__':
-    # Replace 'YOUR_API_KEY' with your actual Pexels API key
     api_key = 'lMHAs8u41325vhL5vEiH0l8MjiRxhuQ8wP8D7uNVVJ9dTnKlVAXLwUuj'
-    query = 'nature'  # Replace with your desired query
+    query = 'coding'
     num_images = 5
-
     images = get_pexels_images(api_key, query, num_images)
     create_image_collage(images)
